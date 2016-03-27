3bada9daf9f178b77b1b661444e4da14e48bb613
trikitrok <trikitrok@gmail.com>
2016-03-27 06:06:15 +0200

    Added required default scopes and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_one :profile
   has_many :todo_lists
   has_many :todo_items, through: :todo_lists, source: :todo_items
+
+  validates :username, presence: true
 end
