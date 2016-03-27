eaaa4ebec2592c20f048fd95519aa1b9e08e3be5
trikitrok <trikitrok@gmail.com>
2016-03-27 06:43:03 +0200

    Counting completed todo items

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   has_many :todo_items, dependent: :destroy, through: :todo_lists, source: :todo_items
 
   validates :username, presence: true
+
+  def get_completed_count
+    self.todo_items.select { |item| item.completed == true }.count
+  end
 end
