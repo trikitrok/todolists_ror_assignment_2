c9c616849aaa05f2325600c85e9218d5812df044
trikitrok <trikitrok@gmail.com>
2016-03-27 06:08:27 +0200

    Added cascade deletes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  has_one :profile
-  has_many :todo_lists
-  has_many :todo_items, through: :todo_lists, source: :todo_items
+  has_one :profile, dependent: :destroy
+  has_many :todo_lists, dependent: :destroy
+  has_many :todo_items, dependent: :destroy, through: :todo_lists, source: :todo_items
 
   validates :username, presence: true
 end
